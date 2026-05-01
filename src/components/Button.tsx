bf105e9a171c8abb61d003252877e957198e130f
Scapranger <36254545+RefinedSaltqwe@users.noreply.github.com>
2026-04-30 21:33:46 -0600

    buttons, pictures, links

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,33 +1,37 @@
 import { classed } from "@tw-classed/react";
 
-export const Button = classed("button", "w-full rounded-md mt-4 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", {
+export const Button = classed(
+  "button",
+  "w-full rounded-md mt-4 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#cd9b3c] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#b8860b]",
+  {
     variants: {
-        size: {
-          sm: "text-sm",
-          md: "text-md",
-          lg: "text-lg",
-        },
-        color: {
-          primary: "bg-indigo-600",
-          secondary: "bg-red-500",
-        },
+      size: {
+        sm: "text-sm",
+        md: "text-md",
+        lg: "text-lg",
       },
-     
-      compoundVariants: [
-        {
-          size: "sm",
-          color: "secondary",
-          class: "px-2 py-1",
-        },
-        {
-          size: "md",
-          color: "secondary",
-          class: "px-4 py-2",
-        },
-      ],
-     
-      defaultVariants: {
-        size: "md",
-        color: "primary",
+      color: {
+        primary: "bg-[#b8860b]",
+        secondary: "bg-red-500",
+      },
     },
-})
+
+    compoundVariants: [
+      {
+        size: "sm",
+        color: "secondary",
+        class: "px-2 py-1",
+      },
+      {
+        size: "md",
+        color: "secondary",
+        class: "px-4 py-2",
+      },
+    ],
+
+    defaultVariants: {
+      size: "md",
+      color: "primary",
+    },
+  },
+);
